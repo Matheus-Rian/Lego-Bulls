26b406051901dfc3175a3da1bb88bf8d1aa66f1d
Matheus Rian <souzamatheusrian@gmail.com>
2021-05-04 16:14:51 -0300

    feat: add components in home module ✨

--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -1,3 +1,4 @@
+import { TopicComponent } from './../../shared/topic/topic.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -7,6 +8,8 @@ import { HeaderComponent } from 'src/app/components/header/header.component';
 import { WelcomeComponent } from 'src/app/components/sections/welcome/welcome.component';
 import { ProjectsComponent } from 'src/app/components/sections/projects/projects.component';
 import { AwardsComponent } from 'src/app/components/sections/awards/awards.component';
+import { CardAwardComponent } from 'src/app/shared/card-award/card-award.component';
+import { FooterComponent } from 'src/app/components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +18,9 @@ import { AwardsComponent } from 'src/app/components/sections/awards/awards.compo
     WelcomeComponent,
     ProjectsComponent,
     AwardsComponent,
+    TopicComponent,
+    CardAwardComponent,
+    FooterComponent,
   ],
   imports: [
     CommonModule,
